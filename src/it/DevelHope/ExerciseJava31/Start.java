c5d198dedd933ad3097887edd20a75dbc58df1fd
Marco Lo Bello <xSalamander@outlook.it>
2022-12-19 17:45:22 +0100

    Initial Commit

---
+++ src/it/DevelHope/ExerciseJava31/Start.java
@@ -0,0 +1,19 @@
+package it.DevelHope.ExerciseJava31;
+
+public class Start {
+    public static void main(String[] args) {
+
+        //DEFINE ARRAY1 WITH THE STRINGS " I want ", " to learn " AND " how to code! ".
+        String[] array1 = {" I want ", " to learn ", " how to code! "};
+        String[] array2 = {array1[0].trim(), array1[1].trim(), array1[2].trim()};
+
+        int statement1 = array2[0].compareTo("I want");
+        int statement2 = array2[1].compareToIgnoreCase("To learn");
+
+        boolean result = !(statement1 > statement2);
+
+        System.out.println(result);
+
+
+    }
+}
